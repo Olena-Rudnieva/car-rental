fc899b21a69d644b468088f8d4e2c61d0e66cb4b
Olena-Rudnieva <120596329+Olena-Rudnieva@users.noreply.github.com>
2023-12-03 22:01:58 +0000

    Deploying to gh-pages from @ Olena-Rudnieva/car-rental@ca29a4ddc83afef4de659c72d0110f7cd80c5322 🚀

--- static/js/733.faef6301.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[733],{3733:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{children:"Favorite"})}}}]);
-//# sourceMappingURL=733.faef6301.chunk.js.map
